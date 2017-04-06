bc085564a1ab404f20a5d7f6a7e9a380a7673f31
deadlydog <deadlydog@hotmail.com>
2017-04-06 00:33:12 -0600

    cleanup: Convert spaces to tabs

--- publishTools/HelperScripts/Publish-NewReleaseToGitHub.ps1
+++ publishTools/HelperScripts/Publish-NewReleaseToGitHub.ps1
@@ -1,61 +1,61 @@
 function Publish-NewReleaseToGitHub($gitHubReleaseParameters)
 {
-    Install-Module -Name New-GitHubRelease -Scope CurrentUser -Force
+	Install-Module -Name New-GitHubRelease -Scope CurrentUser -Force
 
-    $gitHubAccessTokenEnvironmentalVariableName = 'GitHubAccessToken'
-    $gitHubAccessToken = $gitHubReleaseParameters.GitHubAccessToken
+	$gitHubAccessTokenEnvironmentalVariableName = 'GitHubAccessToken'
+	$gitHubAccessToken = $gitHubReleaseParameters.GitHubAccessToken
 
-    # If a GitHub Access Token was not provided, check the Environmental variable for it. If it's not there either, prompt the user for it.
-    $isGitHubAccessTokenProvidedFromPrompt = $false
-    if ([string]::IsNullOrWhiteSpace($gitHubAccessToken))
-    {
-        $encodedGitHubAccessToken = [Environment]::GetEnvironmentVariable($gitHubAccessTokenEnvironmentalVariableName, "User")
-        if (![string]::IsNullOrWhiteSpace($encodedGitHubAccessToken))
-        {
+	# If a GitHub Access Token was not provided, check the Environmental variable for it. If it's not there either, prompt the user for it.
+	$isGitHubAccessTokenProvidedFromPrompt = $false
+	if ([string]::IsNullOrWhiteSpace($gitHubAccessToken))
+	{
+		$encodedGitHubAccessToken = [Environment]::GetEnvironmentVariable($gitHubAccessTokenEnvironmentalVariableName, "User")
+		if (![string]::IsNullOrWhiteSpace($encodedGitHubAccessToken))
+		{
 			$gitHubAccessTokenAsBytes = [System.Convert]::FromBase64String($encodedGitHubAccessToken)
-            $gitHubAccessToken = [System.Text.Encoding]::UTF8.GetString($gitHubAccessTokenAsBytes)
-        }
+			$gitHubAccessToken = [System.Text.Encoding]::UTF8.GetString($gitHubAccessTokenAsBytes)
+		}
 
-        if ([string]::IsNullOrWhiteSpace($gitHubAccessToken))
-        {
-            $gitHubAccessToken = Read-InputBoxDialog -WindowTitle 'GitHub Access Token Required' -Message 'Please enter your GitHub Access Token:'
-            $isGitHubAccessTokenProvidedFromPrompt = $true
-        }
-    }
+		if ([string]::IsNullOrWhiteSpace($gitHubAccessToken))
+		{
+			$gitHubAccessToken = Read-InputBoxDialog -WindowTitle 'GitHub Access Token Required' -Message 'Please enter your GitHub Access Token:'
+			$isGitHubAccessTokenProvidedFromPrompt = $true
+		}
+	}
 
-    if ([string]::IsNullOrWhiteSpace($gitHubAccessToken))
-    {
-        throw 'No GitHub Access Token was provided, so exiting without attempting to publish a new Release.'
-    }
-    $gitHubReleaseParameters.GitHubAccessToken = $gitHubAccessToken
+	if ([string]::IsNullOrWhiteSpace($gitHubAccessToken))
+	{
+		throw 'No GitHub Access Token was provided, so exiting without attempting to publish a new Release.'
+	}
+	$gitHubReleaseParameters.GitHubAccessToken = $gitHubAccessToken
 
-    # Publish the new version of the module to GitHub.
-    Write-Output "Creating new GitHub release..."
-    $gitHubReleaseCreationResult = New-GitHubRelease @gitHubReleaseParameters
+	# Publish the new version of the module to GitHub.
+	Write-Output "Creating new GitHub release..."
+	$gitHubReleaseCreationResult = New-GitHubRelease @gitHubReleaseParameters
 
-    # If we prompted the user for the Access Token and the publish succeeded, ask them if they want to save it for next time.
-    if ($isGitHubAccessTokenProvidedFromPrompt -and $gitHubReleaseCreationResult.ReleaseCreationSucceeded)
-    {
-        $saveGitHubAccessKeyAnswer = Read-MessageBoxDialog -WindowTitle "Save GitHub Access Token?" -Message 'Would you like to save the GitHub Access Token in an environmental variable so you are not prompted for it next time?' -Buttons YesNo
-        if ($saveGitHubAccessKeyAnswer -eq 'Yes')
-        {
+	# If we prompted the user for the Access Token and the publish succeeded, ask them if they want to save it for next time.
+	if ($isGitHubAccessTokenProvidedFromPrompt -and $gitHubReleaseCreationResult.ReleaseCreationSucceeded)
+	{
+		$saveGitHubAccessKeyAnswer = Read-MessageBoxDialog -WindowTitle "Save GitHub Access Token?" -Message 'Would you like to save the GitHub Access Token in an environmental variable so you are not prompted for it next time?' -Buttons YesNo
+		if ($saveGitHubAccessKeyAnswer -eq 'Yes')
+		{
 			$gitHubAccessTokenAsBytes = [System.Text.Encoding]::UTF8.GetBytes($gitHubAccessToken)
-            $encodedGitHubAccessToken = [System.Convert]::ToBase64String($gitHubAccessTokenAsBytes)
-            [Environment]::SetEnvironmentVariable($gitHubAccessTokenEnvironmentalVariableName, $encodedGitHubAccessToken, "User")
-        }
-    }
+			$encodedGitHubAccessToken = [System.Convert]::ToBase64String($gitHubAccessTokenAsBytes)
+			[Environment]::SetEnvironmentVariable($gitHubAccessTokenEnvironmentalVariableName, $encodedGitHubAccessToken, "User")
+		}
+	}
 
-    # Let the user know if the new Release was created successfully or not.
-    if ($gitHubReleaseCreationResult.Succeeded -eq $true)
-    { 
-        Write-Output "Release published successfully! View it at $($gitHubReleaseCreationResult.ReleaseUrl)"
-    }
-    elseif ($gitHubReleaseCreationResult.ReleaseCreationSucceeded -eq $false)
-    { 
-        throw "The release was not created. Error message is: $($gitHubReleaseCreationResult.ErrorMessage)"
-    }
-    elseif ($gitHubReleaseCreationResult.AllAssetUploadsSucceeded -eq $false)
-    { 
-        throw "The release was created, but not all of the assets were uploaded to it. View it at $($gitHubReleaseCreationResult.ReleaseUrl). Error message is: $($gitHubReleaseCreationResult.ErrorMessage)"
-    }
+	# Let the user know if the new Release was created successfully or not.
+	if ($gitHubReleaseCreationResult.Succeeded -eq $true)
+	{ 
+		Write-Output "Release published successfully! View it at $($gitHubReleaseCreationResult.ReleaseUrl)"
+	}
+	elseif ($gitHubReleaseCreationResult.ReleaseCreationSucceeded -eq $false)
+	{ 
+		throw "The release was not created. Error message is: $($gitHubReleaseCreationResult.ErrorMessage)"
+	}
+	elseif ($gitHubReleaseCreationResult.AllAssetUploadsSucceeded -eq $false)
+	{ 
+		throw "The release was created, but not all of the assets were uploaded to it. View it at $($gitHubReleaseCreationResult.ReleaseUrl). Error message is: $($gitHubReleaseCreationResult.ErrorMessage)"
+	}
 }
--- publishTools/HelperScripts/Publish-NewVersionToPowerShellGallery.ps1
+++ publishTools/HelperScripts/Publish-NewVersionToPowerShellGallery.ps1
@@ -1,59 +1,59 @@
 function Publish-ToPowerShellGallery([string] $moduleDirectoryPath, [string] $powerShellGalleryNuGetApiKey, [bool] $isTestingThisScript)
 {
-    $powerShellGalleryNuGetApiKeyEnvironmentalVariableName = 'PowerShellGalleryNuGetApiKey'
+	$powerShellGalleryNuGetApiKeyEnvironmentalVariableName = 'PowerShellGalleryNuGetApiKey'
 
-    # If a PowerShell NuGet API Key was not provided, check the Environmental variable for it. If it's not there either, prompt the user for it.
-    $isPowerShellGalleryNuGetApiKeyProvidedFromPrompt = $false
-    if ([string]::IsNullOrWhiteSpace($powerShellGalleryNuGetApiKey))
-    {
-        $encodedPowerShellGalleryNuGetApiKey = [Environment]::GetEnvironmentVariable($powerShellGalleryNuGetApiKeyEnvironmentalVariableName, "User")
-        if (![string]::IsNullOrWhiteSpace($encodedPowerShellGalleryNuGetApiKey))
-        {
-            $powerShellGalleryNuGetApiKeyAsBytes = [System.Convert]::FromBase64String($encodedPowerShellGalleryNuGetApiKey)
-            $powerShellGalleryNuGetApiKey = [System.Text.Encoding]::UTF8.GetString($powerShellGalleryNuGetApiKeyAsBytes)
-        }
+	# If a PowerShell NuGet API Key was not provided, check the Environmental variable for it. If it's not there either, prompt the user for it.
+	$isPowerShellGalleryNuGetApiKeyProvidedFromPrompt = $false
+	if ([string]::IsNullOrWhiteSpace($powerShellGalleryNuGetApiKey))
+	{
+		$encodedPowerShellGalleryNuGetApiKey = [Environment]::GetEnvironmentVariable($powerShellGalleryNuGetApiKeyEnvironmentalVariableName, "User")
+		if (![string]::IsNullOrWhiteSpace($encodedPowerShellGalleryNuGetApiKey))
+		{
+			$powerShellGalleryNuGetApiKeyAsBytes = [System.Convert]::FromBase64String($encodedPowerShellGalleryNuGetApiKey)
+			$powerShellGalleryNuGetApiKey = [System.Text.Encoding]::UTF8.GetString($powerShellGalleryNuGetApiKeyAsBytes)
+		}
 
-        if ([string]::IsNullOrWhiteSpace($powerShellGalleryNuGetApiKey))
-        {
-            $powerShellGalleryNuGetApiKey = Read-InputBoxDialog -WindowTitle 'PowerShell Gallery API Key Required' -Message 'Please enter your PowerShell Gallery API Key:'
-            $isPowerShellGalleryNuGetApiKeyProvidedFromPrompt = $true
-        }
-    }
+		if ([string]::IsNullOrWhiteSpace($powerShellGalleryNuGetApiKey))
+		{
+			$powerShellGalleryNuGetApiKey = Read-InputBoxDialog -WindowTitle 'PowerShell Gallery API Key Required' -Message 'Please enter your PowerShell Gallery API Key:'
+			$isPowerShellGalleryNuGetApiKeyProvidedFromPrompt = $true
+		}
+	}
 
-    if ([string]::IsNullOrWhiteSpace($powerShellGalleryNuGetApiKey))
-    {
-        throw 'No PowerShell Gallery API key was provided, so exiting without attempting to publish a new NuGet package.'
-    }
+	if ([string]::IsNullOrWhiteSpace($powerShellGalleryNuGetApiKey))
+	{
+		throw 'No PowerShell Gallery API key was provided, so exiting without attempting to publish a new NuGet package.'
+	}
 
-    if ($isTestingThisScript)
-    {
-        Write-Output "Script is in TESTING mode, so we will not actually try to publish the new NuGet package to the PowerShell Gallery."
-        return
-    }
+	if ($isTestingThisScript)
+	{
+		Write-Output "Script is in TESTING mode, so we will not actually try to publish the new NuGet package to the PowerShell Gallery."
+		return
+	}
 
-    # Publish the new version of the module to the PowerShell Gallery.
-    Write-Output "Publishing new NuGet package to the PowerShell Gallery..."
-    try
-    {
-        Publish-Module -Path $moduleDirectoryPath -NuGetApiKey $powerShellGalleryNuGetApiKey
-    }
-    catch
-    {
-        throw $_.Exception.Message
-    }
+	# Publish the new version of the module to the PowerShell Gallery.
+	Write-Output "Publishing new NuGet package to the PowerShell Gallery..."
+	try
+	{
+		Publish-Module -Path $moduleDirectoryPath -NuGetApiKey $powerShellGalleryNuGetApiKey
+	}
+	catch
+	{
+		throw $_.Exception.Message
+	}
 
-    $powerShellGalleryNuGetPackageExpectedUrl = "$powerShellGalleryNuGetPackageUrlWithTrailingSlash$newVersionNumber"
-    Write-Output "PowerShell Gallery NuGet Package has been published. View it at:  $powerShellGalleryNuGetPackageExpectedUrl"
+	$powerShellGalleryNuGetPackageExpectedUrl = "$powerShellGalleryNuGetPackageUrlWithTrailingSlash$newVersionNumber"
+	Write-Output "PowerShell Gallery NuGet Package has been published. View it at:  $powerShellGalleryNuGetPackageExpectedUrl"
 
-    # If we prompted the user for the API key, ask them if they want to save it for next time.
-    if ($isPowerShellGalleryNuGetApiKeyProvidedFromPrompt)
-    {
-        $savePowerShellGalleryApiKeyAnswer = Read-MessageBoxDialog -WindowTitle "Save PowerShell Gallery API Key?" -Message 'Would you like to save the API key in an environmental variable so you are not prompted for it next time?'-Buttons YesNo
-        if ($savePowerShellGalleryApiKeyAnswer -eq 'Yes')
-        {
-            $powerShellGalleryNuGetApiKeyAsBytes = [System.Text.Encoding]::UTF8.GetBytes($powerShellGalleryNuGetApiKey)
-            $encodedPowerShellGalleryNuGetApiKey = [System.Convert]::ToBase64String($powerShellGalleryNuGetApiKeyAsBytes)
-            [Environment]::SetEnvironmentVariable($powerShellGalleryNuGetApiKeyEnvironmentalVariableName, $encodedPowerShellGalleryNuGetApiKey, "User")
-        }
-    }
+	# If we prompted the user for the API key, ask them if they want to save it for next time.
+	if ($isPowerShellGalleryNuGetApiKeyProvidedFromPrompt)
+	{
+		$savePowerShellGalleryApiKeyAnswer = Read-MessageBoxDialog -WindowTitle "Save PowerShell Gallery API Key?" -Message 'Would you like to save the API key in an environmental variable so you are not prompted for it next time?'-Buttons YesNo
+		if ($savePowerShellGalleryApiKeyAnswer -eq 'Yes')
+		{
+			$powerShellGalleryNuGetApiKeyAsBytes = [System.Text.Encoding]::UTF8.GetBytes($powerShellGalleryNuGetApiKey)
+			$encodedPowerShellGalleryNuGetApiKey = [System.Convert]::ToBase64String($powerShellGalleryNuGetApiKeyAsBytes)
+			[Environment]::SetEnvironmentVariable($powerShellGalleryNuGetApiKeyEnvironmentalVariableName, $encodedPowerShellGalleryNuGetApiKey, "User")
+		}
+	}
 }
